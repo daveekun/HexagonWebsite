40e1c76981589937a847ea2a22309a91459d4756
daveekun <teaelvtars.work@gmail.com>
2024-03-17 23:36:35 +0200

    people added

--- src/Board.js
+++ src/Board.js
@@ -1,5 +1,10 @@
 import PersonHexagon from "./Hexagons/PersonHexagon.js";
 import SectionTitle from "./SectionTitle.js";
+import person0 from './people/person0.png'
+import person1 from './people/person1.png'
+import person2 from './people/person2.png'
+import person3 from './people/person3.png'
+import person4 from './people/person4.png'
 function BoardSection()
 {
     return (
@@ -7,13 +12,13 @@ function BoardSection()
             <SectionTitle title="Our-Board" />
             <div className="container">
                 <div className="hrow hexagonrow1">
-                    <PersonHexagon />
-                    <PersonHexagon />
-                    <PersonHexagon />
-                    <PersonHexagon />
+                    <PersonHexagon title="External Affairs Manager" src={ person1 } />
+                    <PersonHexagon title="President" src={ person0 } />
+                    <PersonHexagon title="Vice President" src={ person2 } />
+                    <PersonHexagon title="Internal Affairs Manager" src={ person3 } />
                 </div>
                 <div className="hrow hexagonrow2">
-                    <PersonHexagon />
+                    <PersonHexagon title="Event Manager" src={ person4 } />
                     <PersonHexagon />
                     <PersonHexagon />
                 </div>
--- src/Hexagons/PersonHexagon.js
+++ src/Hexagons/PersonHexagon.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import hexagon from '../hexagon.png'
 import jordan from './Untitled-1.png'
-export default function PersonHexagon() {
+export default function PersonHexagon(props) {
     return (
         <div className='personhexagon'>
             <img className="hexagonbackground" src={ hexagon } />
-            <p className='persontitle'>The IT guy</p>
-            <img className='portrait' src={ jordan } />
+            <p className='persontitle'>{ props.title }</p>
+            <img className='portrait' src={ props.src } />
         </div>
     )
 }
